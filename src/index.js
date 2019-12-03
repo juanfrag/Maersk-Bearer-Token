f213dd89667948b27118995d52a1544189fb3125
Luis <luedblra@gmail.com>
2019-12-03 09:31:46 -0400

    prueba inmediata

--- src/index.js
+++ src/index.js
@@ -39,8 +39,9 @@ async function run() {
     });*/
     const page = await browser.newPage();
     await page.goto('https://www.maersk.com/portaluser/login');//, {waitUntil: ['domcontentloaded', 'networkidle0'], timeout: 0});
-    await page.setDefaultNavigationTimeout(0); 
-
+    //await page.setDefaultNavigationTimeout(0); 
+    console.log(await page.content());
+    await page.waitFor(5000);
 
     const USERNAME_SELECTOR = '#usernameInput';
     const PASSWORD_SELECTOR = '#passwordInput';
